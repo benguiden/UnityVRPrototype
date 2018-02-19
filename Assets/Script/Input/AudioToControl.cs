44481141850839599673060f374daed4263dad23
Ben Guiden <benguiden@hotmail.co.uk>
2018-02-19 23:00:33 +0000

    Updated project to Unity 2017.3.1f1. Added microphone input.

---
+++ Assets/Script/Input/AudioToControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioToControl : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public Transform[] points;
+    public float k = 200f;
+
+    void Awake() {
+        audioSource = GetComponent<AudioSource> ();
+    }
+
+    void Update() {
+        //24000
+        float[] samples = new float[1024];
+        audioSource.GetSpectrumData (samples, 0, FFTWindow.Rectangular);
+        for (int i = 0; i < points.Length; i++) {
+            points[i].localPosition = new Vector3 (points[i].localPosition.x, samples[i * 2] * k, 0f);
+        }
+    }
+
+}
---
+++ Assets/Script/Input/MicrophoneInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneInput : MonoBehaviour {
+
+    private AudioSource audioSource;
+    public string audioDevice;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource> ();
+    }
+
+    private void Start() {
+        if (UnityEngine.Microphone.devices != null) {
+            PrintAudioDevices ();
+            audioDevice = UnityEngine.Microphone.devices[0];
+
+            audioSource.clip = UnityEngine.Microphone.Start (audioDevice, true, 10, 44100);
+            audioSource.loop = true;
+
+            while (!(UnityEngine.Microphone.GetPosition (audioDevice) > 0)) { }
+            audioSource.Play ();
+
+        } else {
+            Debug.LogError ("Error: No audio devices for audio input.");
+            Debug.Break ();
+        }
+    }
+
+    private void PrintAudioDevices() {
+        string output = "Audio Devices: ";
+        for (int i=0; i<UnityEngine.Microphone.devices.Length; i++) {
+            output += UnityEngine.Microphone.devices[i] + ", ";
+        }
+        output += "\n";
+        Debug.Log (output);
+    }
+
+}
