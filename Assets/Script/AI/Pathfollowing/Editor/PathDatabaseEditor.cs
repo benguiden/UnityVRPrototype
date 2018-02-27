2a4886ac6612acc0641e4320f0b8bea35984d7a3
Ben Guiden <benguiden@hotmail.co.uk>
2018-02-27 12:32:27 +0000

    Fixed path database saving error.

--- Assets/Script/AI/Pathfollowing/Editor/PathDatabaseEditor.cs
+++ Assets/Script/AI/Pathfollowing/Editor/PathDatabaseEditor.cs
@@ -18,6 +18,7 @@ using UnityEditor;
             if (database.paths == null)
                 database.paths = new List<AIPath> ();
 
+        bool valueChanged = false;
             for (int i = 0; i < database.paths.Count; i++) {
 
                 GUI.backgroundColor = new Color (0.6f, 0.6f, 0.5f, 1f);
@@ -32,14 +33,18 @@ using UnityEditor;
                     } else if (GUILayout.Button ("x", EditorStyles.toolbarButton, GUILayout.Width (24))) {
                         database.paths.RemoveAt (i);
                         pathIndex = -1;
-                    }
+                    valueChanged = true;
+                }
                     EditorGUILayout.EndHorizontal ();
 
                     if (pathIndex != -1) {
+                    EditorGUI.BeginChangeCheck();
                         database.paths[i].name = EditorGUILayout.TextField (database.paths[i].name, EditorStyles.toolbarTextField);
 
                         database.paths[i].color = EditorGUILayout.ColorField ("Colour", database.paths[i].color);
-
+                    if (EditorGUI.EndChangeCheck()) {
+                        valueChanged = true;
+                    }
                         int newPointIndex = -1;
 
                         if (database.paths[i].points == null)
@@ -47,13 +52,19 @@ using UnityEditor;
                         for (int j = 0; j < database.paths[i].points.Count; j++) {
                             EditorGUILayout.BeginHorizontal ();
                             EditorGUILayout.LabelField (j.ToString (), GUILayout.Width (16));
+                        EditorGUI.BeginChangeCheck();
                             database.paths[i].points[j] = EditorGUILayout.Vector3Field ("", database.paths[i].points[j]);
+                        if (EditorGUI.EndChangeCheck()) {
+                            valueChanged = true;
+                        }
                             if (GUILayout.Button ("+", EditorStyles.toolbarButton, GUILayout.Width (20))) {
                                 newPointIndex = j;
-                            }
+                            valueChanged = true;
+                        }
                             if (GUILayout.Button ("x", EditorStyles.toolbarButton, GUILayout.Width (20))) {
                                 database.paths[i].points.RemoveAt (j);
-                            }
+                            valueChanged = true;
+                        }
                             EditorGUILayout.EndHorizontal ();
                         }
                         if (newPointIndex >= 0) {
@@ -63,7 +74,8 @@ using UnityEditor;
                                 database.paths[i].points.Add (new Vector3 ());
                             else
                                 database.paths[i].points.Add (database.paths[i].points[database.paths[i].points.Count - 1]);
-                        }
+                        valueChanged = true;
+                    }
                     }
 
                     GUILayout.Space (24);
@@ -74,7 +86,14 @@ using UnityEditor;
             }
             if (GUILayout.Button ("+", EditorStyles.toolbarButton)) {
                 database.paths.Add (new AIPath());
+            valueChanged = true;
             }
+
+            if (valueChanged) {
+            AssetDatabase.Refresh();
+            EditorUtility.SetDirty(database);
+            AssetDatabase.SaveAssets();
+        }
         }
 
         void OnEnable() {
